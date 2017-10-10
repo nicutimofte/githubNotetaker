52a2b8f2d7af2ce6ebd1a7c3124cf8c26d49fccc
Nicu <timofte.alexandru05@yahoo.com>
2017-10-10 20:04:58 +0300

    added firebase rest api

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -8,7 +8,20 @@ let api = {
 		userName = userName.toLowerCase().trim();
 		const url = `https://api.github.com/users/${userName}/repos`;
 		return fetch(url).then((res) => res.json());
+	},
+	getNotes(userName) {
+		userName = userName.toLowerCase().trim();
+		const url = `https://githubnotetaker-reactnative.firebaseio.com/${userName}.json`;
+		return fetch(url).then((res) => res.json());
+	},
+	addNote(userName, note) {
+		userName = userName.toLowerCase().trim();
+		const url = `https://githubnotetaker-reactnative.firebaseio.com/${userName}.json`;
+		return fetch(url,{
+			method: 'post',
+			body: JSON.stringify(note),
+		}).then((res) => res.json());
 	}
-}
+} 
 
 module.exports = api
